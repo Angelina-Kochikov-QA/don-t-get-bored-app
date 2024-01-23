576dca709daec86e13554486e7be4736b8e9d976
“Angelina <mrs.angelinach@gmail.com>
2024-01-22 22:37:28 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import icon from './sleeping_6943844-removebg-preview.png'
+
 
 function App() {
+
+  const [advice, setAdvice] = useState("");
+
+
+
+  useEffect(() => {
+    getAdvice()
+  },[])
+
+  const getAdvice = async () => {
+    const response = await fetch(`http://www.boredapi.com/api/activity/`);
+    const data = await response.json();
+    setAdvice(data.activity)
+    
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1>
+          Getting bored? <img className='icon' src={icon} width="50px" alt='icon'/>
+          <br></br>Click to find activity to do
+          <br></br>
+        </h1>
+
+        <div className='card'>
+          <p>{advice}</p>
+          </div>
+
+          <div className='container'>
+          <button onClick={getAdvice}>New activity</button>
+            </div>
+
+    
+
+    </div>
     </div>
   );
 }
